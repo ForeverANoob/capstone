97795b4347a08c0b9b40ccebcd3894fdadb9ded3
Andre Barreiros <andresbarreiros2012@gmail.com>
2017-09-29 17:14:40 +0200

    Was busy with the search function, have to find out how to draw out the search term.

--- src/main/java/stumasys/SearchController.java
+++ src/main/java/stumasys/SearchController.java
@@ -3,6 +3,7 @@ package stumasys;
 import java.util.HashMap;
 import java.util.Map;
 import java.security.Principal;
+import java.util.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -17,9 +18,23 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import stumasys.db.Database;
+import stumasys.db.Course;
+import stumasys.db.Assessment;
+import stumasys.db.User;
 
 @Controller
 public class SearchController {
+
+    private Database db;
+
+    @Autowired
+    public void setDatabase(Database db) {
+        this.db = db;
+    }
 
     @RequestMapping(value = "/search/**")
     public String courseHandler(
@@ -37,9 +52,10 @@ public class SearchController {
         return "search";
     }
 
-    @RequestMapping(value = "/api/get_search_results") // TODO: search REST api function that actually searches and gives results, that will be fetched by JS on the /search webpage
+    @RequestMapping(value = "/api/get_search_results", method = RequestMethod.POST) // TODO: search REST api function that actually searches and gives results, that will be fetched by JS on the /search webpage
     public String yieldSearchResults(
-        Principal p
+        @RequestParam("searchTerm")
+        String searchTerm
     ){
         // TODO: get the search results for this search term from the DB class
         // It must return a String-formatted JSON object
@@ -50,9 +66,68 @@ public class SearchController {
         // into a whole bunch of DIVs inside the search page, when you click
         // on them they redirect you to /course/YEAR/COURSE_CODE
 
+        String proxy = "csc1";  // TODO: This will act as the course typed in the search result
+/*
+        String split = proxy.split("_");
 
+        Course c = db.getCourse(proxy[0], Integer.parseint(proxy[1]));
 
-        return "final";
+        if (c == null){
+            return "that course does not exist"
+        }
+
+        List<Assessment> a = c.getAssessments();
+        if (a == null){
+            return "No assessments";
+        }
+        String ret = "[[" + proxy[0]+","+ proxy[1] +"],";
+
+        for (int i = 0; i < c.size(); i++){
+
+            Map<String,Integer> tmp = c.get(i).getWholeTable();
+
+            Iterator<Map.Entry<String,Integer>> entryItr = tmp.entrySet().iterator();
+
+            if (!entryItr.hasNext()) {
+                return "[]";            // not sure about this
+            }
+            ret += "[";
+            // encoding the mark table into JSON and returning it
+            Map.Entry<String,Integer> entry = entryItr.next();
+            ret += "[\"" + entry.getKey() + "\",\"" + entry.getValue().toString() + "]";
+
+            while (entryItr.hasNext()) {
+                entry = entryItr.next();
+                ret += ",[\"" + entry.getKey() + "\",\"" + entry.getValue().toString() + "]";
+            }
+            ret += "]";
+
+        }
+
+        return ret+"]";*/
+        List<Course> cs = null;
+        if (this.isInteger(proxy)){      // distiguish between year and course typed in
+            cs = db.getLikeYear(proxy);
+        }
+        else{
+            cs = db.getLikeCourse(proxy);
+        }
+
+        if (cs == null){
+            return "No course like that was found";
+        }
+
+        String ret = "[";
+
+        for (int i = 0; i < cs.size(); i++){
+            if(i == 0){
+                ret += "[\"" + cs.get(0).getID() + "]";
+            }else{
+                ret += ",[\"" + cs.get(0).getID() + "]";
+            }
+        }
+        ret += "]";
+        return ret;
 
     }
 /*
@@ -64,4 +139,16 @@ public class SearchController {
         return map;
     }
 */
+
+private boolean isInteger(String str) {     // checks if the str is a number
+    int size = str.length();
+
+    for (int i = 0; i < size; i++) {
+        if (!Character.isDigit(str.charAt(i))) {
+            return false;
+        }
+    }
+    return size > 0;
+    }
+
 }
